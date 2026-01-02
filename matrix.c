8eb299dc39f341a6e3f1973865f8b9de1d7b91de
Andrey Marin <andreymarindev@gmail.com>
2026-01-03 01:41:33 +0300

    refactor: add matrix methods

--- matrix.c
+++ matrix.c
@@ -1,25 +1,57 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "matrix.h"
 
- Matrix allocate_matrix(int rows, int columns) {
-    Matrix matrix;
-    matrix.m = malloc(rows * sizeof(int *));
+Matrix *m_alloc(unsigned int rows, unsigned int columns) {
+    if (rows == 0 || columns == 0) return NULL;
 
-    for (int i = 0; i < rows; ++i) {
-        matrix.m[i] = malloc(columns * sizeof(int));
-    }
+    Matrix *matrix = malloc(sizeof(Matrix));
 
-    matrix.rows = rows;
-    matrix.columns = columns;
+    matrix->rows = rows;
+    matrix->columns = columns;
+    matrix->size = rows * columns;
+    matrix->data = calloc(rows * columns, sizeof(int));
 
     return matrix;
 }
 
-void free_matrix(Matrix *matrix) {
-    for (int i = 0; i < matrix->rows; ++i) {
-        free(matrix->m[i]);
+void m_free(Matrix *matrix) {
+    free(matrix->data);
+    free(matrix);
+}
+
+int m_get(Matrix *matrix, unsigned int i, unsigned int j) {
+    if (i < 0 || i >= matrix->rows || j < 0 || j >= matrix->columns) {
+        printf("Error occured attempting to get matrix element");
+        exit(1);
     }
 
-    free(matrix->m);
+    return matrix->data[i * matrix->columns + j];
+}
+
+void m_set(Matrix *matrix, unsigned int i, unsigned int j, int value) {
+    if (i < 0 || i >= matrix->rows || j < 0 || j >= matrix->columns) {
+        printf("Error occured attempting to set matrix element");
+        exit(1);
+    }
+
+    matrix->data[i * matrix->columns + j] = value;
+}
+
+int m_comp(Matrix *matrix, unsigned int i1, unsigned int j1, unsigned int i2, unsigned int j2) {
+    int v1 = m_get(matrix, i1, j1);
+    int v2 = m_get(matrix, i2, j2);
+
+    return v1 - v2;
+}
+
+void m_print(Matrix *matrix) {
+    for (int i = 0; i < matrix->rows; ++i) {
+        for (int j = 0; j < matrix->columns; ++j) {
+            printf("%d ", matrix->data[i * matrix->columns + j]);
+        }
+
+        printf("\n");
+    }
 }
--- matrix.h
+++ matrix.h
@@ -2,12 +2,17 @@
 #define cdiff_matrix_h
 
 typedef struct {
-    int **m;
+    int *data;
     int rows;
     int columns;
+    int size;
 } Matrix;
 
-Matrix allocate_matrix(int rows, int columns);
-void free_matrix(Matrix *matrix);
+Matrix *m_alloc(unsigned int rows, unsigned int columns);
+int m_get(Matrix *matrix, unsigned int i, unsigned int j);
+void m_set(Matrix *matrix, unsigned int i, unsigned int j, int value);
+int m_comp(Matrix *matrix, unsigned int i1, unsigned int j1, unsigned int i2, unsigned int j2);
+void m_free(Matrix *matrix);
+void m_print(Matrix *matrix);
 
 #endif
